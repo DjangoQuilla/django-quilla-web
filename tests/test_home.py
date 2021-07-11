dbe4608618c760990234d063d5d8f13109d1fa2d
Sergio C. Orozco Torres <scot3004@gmail.com>
2021-07-10 21:17:32 -0500

    Remove piptest, throws errors on netlify

--- tests/test_home.py
+++ tests/test_home.py
@@ -7,5 +7,5 @@ def test_title(selenium, base_url):
 @pytest.mark.nondestructive
 def test_main_heading(selenium, base_url):
     selenium.get(base_url)
-    assert selenium.find_element_by_tag_name('h1').text == "Python Barranquilla."
+    assert selenium.find_element_by_tag_name('h1').text == "Python Medellin."
 
